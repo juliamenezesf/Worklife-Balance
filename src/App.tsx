544f44d9db4918bbb7f4a50ec72ab3ab7b284abf
juliamenezesf <juliakauanemenezesf@gmail.com>
2025-11-19 10:16:54 -0300

    chore: setup Vite + TS + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-800">
+      <h1 className="text-5xl font-extrabold text-white">
+        Tailwind funcionando 😎
+      </h1>
+    </div>
+  );
+}
+
+export default App;
